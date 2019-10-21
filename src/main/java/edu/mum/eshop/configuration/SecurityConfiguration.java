68ec2090598d8e744f06033e503608204587f579
SajuAhmad <msahmad@mum.edu>
2019-10-21 16:29:36 -0500

    Improved spring security file

--- src/main/java/edu/mum/eshop/configuration/SecurityConfiguration.java
+++ src/main/java/edu/mum/eshop/configuration/SecurityConfiguration.java
@@ -2,19 +2,24 @@ package edu.mum.eshop.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -42,34 +47,34 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
-                .antMatchers("/h2-console/**").permitAll()
-                .antMatchers("/").permitAll()
-                .antMatchers("/login").permitAll()
-                .antMatchers("/registration").permitAll()
-                .antMatchers("/buyerRegistration").permitAll()
-                .antMatchers("/sellerRegistration").permitAll()
+                .antMatchers("/", "/login", "/registration", "/buyerRegistration", "/sellerRegistration").permitAll()
+//                .antMatchers("/home").hasAnyAuthority("BUYER", "SELLER").anyRequest().authenticated()
+                .and()
+                .csrf().disable()
+                .formLogin()
+                .loginPage("/login").failureUrl("/login?error=true").defaultSuccessUrl("/home").usernameParameter("email").passwordParameter("password")
+                .and()
+                .logout().invalidateHttpSession(true).deleteCookies("JSESSIONID")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/")
+                .and()
+                .exceptionHandling().accessDeniedPage("/access-denied")
+                .and()
+                .rememberMe().rememberMeParameter("remember-me").tokenRepository(tokenRepository());
 
-                .antMatchers("/products/").permitAll()
-
-                .antMatchers("/home").hasAnyAuthority("BUYER", "SELLER").anyRequest()
-
-                .authenticated().and().csrf().disable().formLogin()
-                .loginPage("/login").failureUrl("/login?error=true")
-                .defaultSuccessUrl("/home")
-                .usernameParameter("email")
-                .passwordParameter("password")
-                .and().logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/").and().exceptionHandling()
-                .accessDeniedPage("/access-denied");
-
+        http.rememberMe().rememberMeParameter("remember-me").key("uniqueAndSecret");
         http.headers().frameOptions().disable();
     }
 
     @Override
     public void configure(WebSecurity web) {
-        web.ignoring()
-                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/img/**", "/media/**", "/scss/**", "/fonts/**");
+        web.ignoring().antMatchers("/resources/**", "/static/**", "/vendor/**", "/css/**", "/js/**", "/img/**", "/media/**", "/scss/**", "/fonts/**");
+    }
+
+    @Bean
+    public PersistentTokenRepository tokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepositoryImpl = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepositoryImpl.setDataSource(dataSource);
+        return jdbcTokenRepositoryImpl;
     }
 
 }
