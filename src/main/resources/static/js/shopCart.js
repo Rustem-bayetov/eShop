5bbe464d8403f9b186804a42c3bc593cf983db31
Russ <rustem.bayetov@gmail.com>
2019-10-22 14:39:27 -0500

    Added shopping cart to main menu

---
+++ src/main/resources/static/js/shopCart.js
@@ -0,0 +1,88 @@
+$(function () {
+    reloadMenuShoppingCart();
+    $("#shopping-cart").on("click", ".delete-item-from-cart", deleteFromMenuShoppingCart);
+});
+
+function reloadMenuShoppingCart() {
+    $.post("/shoppingcart/getCart")
+        .done(function (data) {
+            showMenuShoppingCart(data);
+        });
+}
+
+function showMenuShoppingCart(data) {
+    $("#shopping-cart").html("");
+    let itemsCount = 0;
+
+    if (data) {
+        for (const item of data.items) {
+            let li = $("<li>").addClass("dropdown-cart__item");
+            let div = $("<div>").addClass("media");
+            div.append($("<img>").addClass("dropdown-cart__img").attr("src", item.product.imageUrl).attr("alt", item.product.title));
+            let divBody = $("<div>").addClass("media-body pl-3");
+            divBody.append($("<a>").attr("href", "#").addClass("h6").text(item.product.title));
+            divBody.append($("<span>").addClass("text-primary").text("$" + item.product.price));
+            if (item.quantity > 1) {
+                divBody.append($("<span>").addClass("text-secondary").text(" (x" + item.quantity + ")"));
+            }
+            div.append(divBody);
+            li.append(div);
+
+            let a = $("<a>").addClass("dropdown-cart__item-remove").attr("href", "#");
+            a.append($("<i>").addClass("ti-close delete-item-from-cart").attr("item-id", item.product.id));
+            li.append(a);
+
+            $("#shopping-cart").append(li);
+            itemsCount++;
+        }
+    }
+
+    if (itemsCount === 0) {
+        menuShoppingCartEmpty();
+    } else {
+        let liTotal = `<li class="px-2 py-4 text-center">Subtotal: <span class="text-primary font-weight-semiBold"> $${data.totalSum}</span></li>`;
+        $("#shopping-cart").append(liTotal);
+
+        let li = $("<li>").addClass("dropdown-cart__item");
+        let div = $("<div>").addClass("media");
+        let divBody = $("<div>").addClass("media-body pl-3");
+        divBody.append($("<a>").addClass("btn btn-outline-primary mx-1").attr("href", "/shoppingcart/").text("View Cart"));
+        divBody.append($("<a>").addClass("btn btn-primary mx-1").attr("href", "/shoppingcart/checkout").text("Checkout"));
+        div.append(divBody);
+        li.append(div);
+
+        $("#shopping-cart").append(li);
+    }
+}
+
+function menuShoppingCartEmpty() {
+    $("#shopping-cart").html("");
+
+    let li = $("<li>").addClass("dropdown-cart__item");
+    let div = $("<div>").addClass("media");
+    let divBody = $("<div>").addClass("media-body pl-3");
+    divBody.append($("<p>").addClass("h6").text("Your shopping cart is empty"));
+    div.append(divBody);
+    li.append(div);
+    $("#shopping-cart").append(li);
+}
+
+function deleteFromMenuShoppingCart(e) {
+    e.preventDefault();
+
+    $.ajax({
+        type: "DELETE",
+        url: "/shoppingcart/?productId=" + this.getAttribute("item-id"),
+        contentType: "application/json",
+        dataType: "json",
+        success: function (data) {
+            NotificationSuccess("Item deleted from your cart");
+            reloadMenuShoppingCart();
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            console.log(XMLHttpRequest.responseJSON);
+        }
+    });
+
+    return false;
+}
---
+++ src/main/resources/static/js/site.js
@@ -0,0 +1,98 @@
+﻿function ProceedResult(result, nullDataMessage = "") {
+    if (!result) {
+        if (nullDataMessage !== "") {
+            NotificationError(nullDataMessage);
+        } else {
+            NotificationError("Can't reach server.");
+        }
+
+        return false;
+    } else {
+        if (result.success) {
+            return true;
+        } else {
+            if (result.errors) {
+                result.errors.forEach(function (error) {
+                    NotificationError(error);
+                });
+            }
+            return false;
+        }
+    }
+}
+
+$.notifyDefaults({
+    placement: {
+        from: "bottom"
+    },
+    animate: {
+        enter: "animated fadeInUp",
+        exit: "animated fadeOutDown"
+    }
+});
+
+function NotificationError(message) {
+    $.notify({
+        icon: 'fa fa-exclamation-triangle',
+        title: "&nbsp;&nbsp;",
+        message: message
+    },
+        {
+            type: 'danger'
+        });
+}
+
+function NotificationWarning(message) {
+    $.notify({
+        message: message
+    },
+        {
+            type: 'warning'
+        });
+}
+
+function NotificationInfo(message) {
+    $.notify({
+        message: message
+    });
+}
+
+function NotificationSuccess(message) {
+    $.notify({
+        icon: 'fa fa-check',
+        title: "&nbsp;&nbsp;",
+        message: message
+    },
+        {
+            type: 'success'
+        });
+}
+
+(function ($) {
+    $.fn.buttonLoader = function (action, resetIcon) {
+        var self = $(this);
+        //start loading animation
+        if (action == 'start') {
+            if ($(self).attr("disabled") == "disabled") {
+                e.preventDefault();
+            }
+            //disable buttons when loading state
+            $('.has-spinner').attr("disabled", "disabled");
+            //$(self).attr('data-btn-text', $(self).text());
+            //binding spinner element to button and changing button text
+            $(self).html('<span class="spinner"><i class="fa fa-spinner fa-spin fa-fw"></i></span>' + $(self).text());
+            //$(self).html('<span class="spinner"><i class="fa fa-spinner fa-pulse fa-fw"></i></span>Loading');
+            $(self).addClass('active');
+        }
+        //stop loading animation
+        if (action == 'stop') {
+            //$(self).html($(self).attr('data-btn-text'));
+            $(self).removeClass('active');
+            //enable buttons after finish loading
+            $('.has-spinner').removeAttr("disabled");
+            if (resetIcon){
+                $(self).html(`<i class="${resetIcon}"></i>` + $(self).text());
+            }
+        }
+    }
+})(jQuery);
