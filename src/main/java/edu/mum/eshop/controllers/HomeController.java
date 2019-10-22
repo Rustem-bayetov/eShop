6f833ba3c8e1a83c05e36b732ca603a1610fcc7f
SajuAhmad <msahmad@mum.edu>
2019-10-21 20:26:19 -0500

    Merge branch 'master' of C:\Users\Admin\Downloads\WAA\Final Project\eShop with conflicts.

--- src/main/java/edu/mum/eshop/controllers/HomeController.java
+++ src/main/java/edu/mum/eshop/controllers/HomeController.java
@@ -1,20 +1,46 @@
 package edu.mum.eshop.controllers;
 
+import edu.mum.eshop.domain.users.User;
+import edu.mum.eshop.services.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import javax.servlet.http.HttpSession;
+import java.security.Principal;
 
 @Controller
+@SessionAttributes("loggedInUser")
 public class HomeController {
+    @Autowired
+    UsersService usersService;
 
     @GetMapping("/")
-    public String login() {
-        return "users/login";
+    public String root() {
+        return "home/index";
     }
 
-    @PreAuthorize("hasAnyAuthority('BUYER', 'SELLER')")
+    @PreAuthorize("hasAnyAuthority('BUYER', 'SELLER', 'ADMIN')")
     @GetMapping("/home")
-    public String index() {
+    public String home(Principal principal, Model model) {
+        model.addAttribute("loggedInUser", usersService.getUserByEmail(principal.getName()));
+//        System.out.println(usersService.getUserByEmail(principal.getName()));
+        return "home/index";
+    }
+
+    @PreAuthorize("hasAuthority('BUYER')")
+    @GetMapping("/buyer")
+    public String buyer() {
+        return "home/index";
+    }
+
+    @PreAuthorize("hasAuthority('SELLER')")
+    @GetMapping("/seller")
+    public String seller() {
         return "home/index";
     }
 }
