83e08838e99f4d12e0927d85c8dfd37c4cdce35e
SajuAhmad <msahmad@mum.edu>
2019-10-23 17:35:53 -0500

    profile (delete), homecontroller modified

--- src/main/java/edu/mum/eshop/controllers/HomeController.java
+++ src/main/java/edu/mum/eshop/controllers/HomeController.java
@@ -1,4 +1,5 @@
 package edu.mum.eshop.controllers;
+
 import edu.mum.eshop.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
@@ -7,22 +8,34 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+
 import java.security.Principal;
 
 @Controller
-// @SessionAttributes("loggedInUser")
 public class HomeController {
-    @Autowired UsersService usersService;
-    @PreAuthorize("hasAnyAuthority('BUYER', 'SELLER', 'ADMIN')")
-    @GetMapping("/home")
-    public String home(Principal principal, Model model) {
-        // model.addAttribute("loggedInUser", usersService.getUserByEmail(principal.getName()));
-        return "home/index"; }
-    @PreAuthorize("hasAuthority('BUYER')") @GetMapping("/buyer")
-    public String buyer() {
+    @Autowired
+    UsersService usersService;
+
+    @GetMapping("/")
+    public String index(){
         return "home/index";
     }
-    @PreAuthorize("hasAuthority('SELLER')") @GetMapping("/seller")
-    public String seller() { return "home/index"; }
+
+//    @PreAuthorize("hasAnyAuthority('BUYER', 'SELLER', 'ADMIN')")
+//    @GetMapping("/home")
+//    public String home(Principal principal, Model model) {
+//        return "home/index";
+//    }
+//
+//    @PreAuthorize("hasAuthority('BUYER')")
+//    @GetMapping("/buyer")
+//    public String buyer() {
+//        return "home/index";
+//    }
+//
+//    @PreAuthorize("hasAuthority('SELLER')")
+//    @GetMapping("/seller")
+//    public String seller() {
+//        return "home/index";
+//    }
 }
